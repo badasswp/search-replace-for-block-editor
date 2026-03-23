2570726af96dbc33e0c87c6bfe0dc6ef2d9cd16d
badasswp <badasswpdev@gmail.com>
2026-03-23 06:14:49 +0000

    Build from release-1.10.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-and-replace',
+        'pretty_version' => 'dev-release-1.10.0-built',
+        'version' => 'dev-release-1.10.0-built',
+        'reference' => '441d520b5b9f3a5841dd8e5ee2fcf8ec7766b2d8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/search-and-replace' => array(
+            'pretty_version' => 'dev-release-1.10.0-built',
+            'version' => 'dev-release-1.10.0-built',
+            'reference' => '441d520b5b9f3a5841dd8e5ee2fcf8ec7766b2d8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

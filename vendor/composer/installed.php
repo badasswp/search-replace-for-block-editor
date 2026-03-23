c09bff62f82a581d3874784a9c29ed91d74d219f
badasswp <badasswpdev@gmail.com>
2026-03-23 07:01:13 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/search-and-replace',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c29541d22263d17600d36161fc17427655eb9b7c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/search-and-replace' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c29541d22263d17600d36161fc17427655eb9b7c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
